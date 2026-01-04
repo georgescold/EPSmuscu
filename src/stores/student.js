20a37cc459abda50e7764f39b064b92d42b8a544
Loys <loyscqlgaming@gmail.com>
2026-01-04 16:09:05 +0100

    fix(store): switch to localStorage for better mobile persistence and fix white page on refresh

--- src/stores/student.js
+++ src/stores/student.js
@@ -2,16 +2,16 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useStudentStore = defineStore('student', () => {
-    const studentInfo = ref(JSON.parse(sessionStorage.getItem('studentInfo')))
+    const studentInfo = ref(JSON.parse(localStorage.getItem('studentInfo')))
 
     function login(info) {
         studentInfo.value = info
-        sessionStorage.setItem('studentInfo', JSON.stringify(info))
+        localStorage.setItem('studentInfo', JSON.stringify(info))
     }
 
     function logout() {
         studentInfo.value = null
-        sessionStorage.removeItem('studentInfo')
+        localStorage.removeItem('studentInfo')
     }
 
     return { studentInfo, login, logout }
